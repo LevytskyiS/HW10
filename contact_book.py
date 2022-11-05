d254383fbcc40bf2634bb54b4de74d7148e1f84c
LevytskyiS <levytskyi.sv@gmail.com>
2022-11-05 19:55:05 +0100

    Update contact_book.py

--- contact_book.py
+++ contact_book.py
@@ -30,29 +30,28 @@ class Phone(Field):
 
 class Record:
 
-    list_of_obj_of_phone = []
-
     def __init__(self, name) -> None:
         self.name = Name(name)
+        self.list_of_obj_of_phone = []
             
     def add_new_phone(self, phone):
         self.phone = Phone(phone)
         self.list_of_obj_of_phone.append(self.phone)
  
     def change_phone(self, phone, new_phone):
-        self.phone = Phone(phone)
+
         self.new_phone = Phone(new_phone)
         for index, number in enumerate(self.list_of_obj_of_phone):
             if number.value == self.phone.value:
-                Record.list_of_obj_of_phone[index] = self.new_phone
+                self.list_of_obj_of_phone[index] = self.new_phone
             else:
                 print(f'The phone {self.phone.value} doesn`t exist')
 
     def delete_phone(self, phone):
-        self.phone = Phone(phone)
+        self.phone = phone  # Ця строка замість self.phone = Phone(phone). Якщо взагалі прибрати, то і пошук не проходить
         for number in self.list_of_obj_of_phone:
-            if self.phone.value == number.value:
-                Record.list_of_obj_of_phone.remove(number)
+            if self.phone == number.value:
+                self.list_of_obj_of_phone.remove(number)
             else:
                 print(f'The number {self.phone.value} cannot be deleted, cause it doesn`t exist.')
 
@@ -65,4 +64,4 @@ user.delete_phone(222)
 
 ab.add_record(user)
 for key, value in ab.data.items():
-    print(key, value.phone.value)
+    print(key, value.phone) #Mark 222
