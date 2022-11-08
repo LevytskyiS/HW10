4d25f8424a0409b8554f974b91cfeff9b3bf4fea
LevytskyiS <levytskyi.sv@gmail.com>
2022-11-08 06:49:16 +0100

    Update contact_book.py

--- contact_book.py
+++ contact_book.py
@@ -9,7 +9,7 @@ class AddressBook(UserDict):
 
     def search_by_name(self, record):
         if len(record.list_of_obj_of_phone) >= 1:
-            return [num for num in record.list_of_obj_of_phone]
+            return [num.value for num in record.list_of_obj_of_phone]
         else: 
             return f'This guy doesn`t have a number.'
     
@@ -17,7 +17,7 @@ class AddressBook(UserDict):
         res = []
         for key, value in self.data.items():
             res.append(key)
-            res.append([num for num in value.list_of_obj_of_phone])
+            res.append([num.value for num in value.list_of_obj_of_phone])
         return res
 
 class Field:
@@ -40,21 +40,21 @@ class Record:
         self.list_of_obj_of_phone = []
             
     def add_new_phone(self, phone):
-        self.list_of_obj_of_phone.append(phone)
+        self.list_of_obj_of_phone.append(Phone(phone))
         return f'The phone was added.'
         
     def change_phone(self, phone, new_phone):
-        for index, number in (enumerate(self.list_of_obj_of_phone)):
-            if phone == number:
-                self.list_of_obj_of_phone[index] = new_phone
+        for values in self.list_of_obj_of_phone:
+            if values.value == phone:
+                values.value = new_phone
                 return f'The number was changed.'
             else:
                 return f'The number {phone} isn`t in your book.'
 
-    def delete_phone(self, phone):
-        for number in self.list_of_obj_of_phone:
-            if phone == number:
-                self.list_of_obj_of_phone.remove(number)
+    def delete_phone(self, number):
+        for values in self.list_of_obj_of_phone:
+            if values.value == number:
+                self.list_of_obj_of_phone.remove(values)
                 return f'The number was deleted successfully.'
             else:
-                return f'The number {phone} isn`t in your book.'
+                return f'The number {number} isn`t in your book.'
